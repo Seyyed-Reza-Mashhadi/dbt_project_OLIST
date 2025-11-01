21aa00a121ce6d6f7059f8a766eda2bbd4be3ee6
Seyyed-Reza-Mashhadi <reza.mashhadi93@gmail.com>
2025-11-02 00:06:52 +0100

    +

--- olist/analyses/test.sql
+++
@@ -1,8 +0,0 @@
--- Checking for duplicates
--- SELECT COUNT(DISTINCT order_id) FROM {{ ref('STG_order_items') }}
--- SELECT COUNT(DISTINCT order_id) FROM {{ ref('INT_order_items_agg') }}
--- SELECT COUNT(DISTINCT order_id) FROM {{ ref('STG_orders') }} 
--- SELECT COUNT(DISTINCT order_id) FROM {{ ref('INT_order_payments_agg') }} 
--- SELECT COUNT(DISTINCT order_id) FROM {{ ref('STG_order_payments') }} 
-
--- SELECT count( * ) AS counters from {{ ref('INT_geolocation') }} 
